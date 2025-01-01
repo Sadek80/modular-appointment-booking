0796aa348add21cf41a333c55ad2bbf7688ec8db
Sadek90 <mohamed.sadek@maxab.io>
2025-01-01 21:51:08 +0200

    feat(Shared-Module): Makes Shared Module Open

--- src/test/java/sadek/doctorAppointments/DoctorAppointmentsApplicationTests.java
+++ src/test/java/sadek/doctorAppointments/DoctorAppointmentsApplicationTests.java
@@ -1,6 +1,5 @@
 package sadek.doctorAppointments;
 
-import com.tngtech.archunit.core.domain.JavaClass;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.modulith.core.ApplicationModules;
