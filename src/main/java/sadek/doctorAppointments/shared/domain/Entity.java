f84be893e95aa2a9116cb63d30136782a15b2d1d
Sadek90 <mohamed.sadek@maxab.io>
2025-01-02 09:19:46 +0200

    refactor(Shared-Module): Refactors Base Entity to has the id field

--- src/main/java/sadek/doctorAppointments/shared/domain/Entity.java
+++ src/main/java/sadek/doctorAppointments/shared/domain/Entity.java
@@ -1,10 +1,17 @@
 package sadek.doctorAppointments.shared.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public abstract class Entity<TID> {
+    @Getter
+    @Setter
+    private TID id;
+
     private final List<IDomainEvent> domainEvents = new ArrayList<>();
 
     public List<IDomainEvent> occurredEvents() {
