0d2332b208b2d4a03acbdf482148847e71072b03
Sadek90 <mohamed.sadek@maxab.io>
2025-01-03 18:23:25 +0200

    feat(DoctorAvailability-Module): Adds DoctorContext

---
+++ src/main/java/sadek/doctorAppointments/doctorAvailability/internal/business/helpers/DoctorContext.java
@@ -0,0 +1,24 @@
+package sadek.doctorAppointments.doctorAvailability.internal.business.helpers;
+
+import org.springframework.stereotype.Component;
+import sadek.doctorAppointments.doctorAvailability.internal.business.models.doctor.Doctor;
+import sadek.doctorAppointments.shared.application.IUserContext;
+
+import java.util.UUID;
+
+@Component
+public class DoctorContext implements IUserContext<Doctor, UUID> {
+    private static final String DOCTOR_ID = "f8c3de3d-1fea-4d7c-a8b0-29f63c4c3454";
+    private static final String DOCTOR_NAME = "sadek";
+    private static final Doctor DEFAULT_DOCTOR =  Doctor.create(DOCTOR_ID, DOCTOR_NAME);
+
+    @Override
+    public Doctor getUser() {
+        return DEFAULT_DOCTOR;
+    }
+
+    @Override
+    public UUID getUserId() {
+        return DEFAULT_DOCTOR.getId().value();
+    }
+}
