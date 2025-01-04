6e7307e164ea01a68dd2ffe345812eaa77b8ca75
Sadek90 <mohamed.sadek@maxab.io>
2025-01-04 09:45:13 +0200

    feat(DoctorAvailability-Module): Adds SlotUpdatedEventHandler

---
+++ src/main/java/sadek/doctorAppointments/doctorAvailability/internal/business/SlotUpdatedEventHandler.java
@@ -0,0 +1,23 @@
+package sadek.doctorAppointments.doctorAvailability.internal.business;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import sadek.doctorAppointments.doctorAvailability.internal.business.events.SlotUpdatedEvent;
+import sadek.doctorAppointments.doctorAvailability.internal.business.services.SlotService;
+import sadek.doctorAppointments.shared.domain.ILogger;
+
+@Service
+@RequiredArgsConstructor
+public class SlotUpdatedEventHandler {
+    private final SlotService slotService;
+    private final ILogger logger;
+
+    @Async
+    @EventListener
+    public void handle(SlotUpdatedEvent event) {
+        logger.warn("Start Slot updated event: " + event);
+
+    }
+}
