3f10d216c6676ab4c97cc74678b5daf81ae85444
Sadek90 <mohamed.sadek@maxab.io>
2025-01-10 21:07:42 +0200

    feat(doctorAppointmentManagement-Module): Adds AppointmentController

---
+++ src/main/java/sadek/doctorAppointments/doctorAppointmentManagement/internal/shell/api/AppointmentController.java
@@ -0,0 +1,40 @@
+package sadek.doctorAppointments.doctorAppointmentManagement.internal.shell.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sadek.doctorAppointments.doctorAppointmentManagement.internal.core.inputPorts.IAppointmentService;
+import sadek.doctorAppointments.doctorAppointmentManagement.internal.core.inputPorts.dto.CancelAppointmentDto;
+import sadek.doctorAppointments.doctorAppointmentManagement.internal.core.inputPorts.dto.CompleteAppointmentDto;
+import sadek.doctorAppointments.shared.presentation.BaseController;
+
+@RestController("DoctorAppointmentManagement-AppointmentController")
+@RequiredArgsConstructor
+public class AppointmentController extends BaseController {
+    private final IAppointmentService appointmentService;
+
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            value = "/appointments/complete"
+    )
+    public ResponseEntity<?> complete(@RequestBody CompleteAppointmentDto request) {
+        return handleResult(appointmentService.completeAppointment(request), HttpStatus.NO_CONTENT);
+    }
+
+    @RequestMapping(
+            method = RequestMethod.PUT,
+            value = "/appointments/cancel"
+    )
+    public ResponseEntity<?> cancel(@RequestBody CancelAppointmentDto request) {
+        return handleResult(appointmentService.cancelAppointment(request), HttpStatus.NO_CONTENT);
+    }
+
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/doctors/appointments"
+    )
+    public ResponseEntity<?> getAllAppointments() {
+        return handleResult(appointmentService.getAppointments(), HttpStatus.OK);
+    }}
