8e5e02247b431529a2ed931025f5acc393601fd7
Sadek90 <mohamed.sadek@maxab.io>
2025-01-10 15:43:43 +0200

    feat(appointmentBooking-Module): Adds AppointmentCreatedDomainEvent

---
+++ src/main/java/sadek/doctorAppointments/appointmentsBooking/internal/domain/appointment/events/AppointmentCreatedDomainEvent.java
@@ -0,0 +1,19 @@
+package sadek.doctorAppointments.appointmentsBooking.internal.domain.appointment.events;
+
+import sadek.doctorAppointments.shared.domain.IDomainEvent;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record AppointmentCreatedDomainEvent(
+        UUID appointmentId,
+        UUID slotId,
+        LocalDateTime startTime,
+        LocalDateTime endTime,
+        Double cost,
+        UUID patientId,
+        String patientName,
+        UUID doctorId,
+        LocalDateTime reservedAt
+) implements IDomainEvent {
+}
