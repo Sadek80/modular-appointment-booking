26ef71b1e3ec8d99fc82a0973fad909748f3391d
Sadek90 <mohamed.sadek@maxab.io>
2025-01-10 16:37:59 +0200

    feat(appointmentBooking-Module): Adds MetaData Package info for public api to be open

---
+++ src/main/java/sadek/doctorAppointments/appointmentsBooking/publicAPI/MetaData.java
@@ -0,0 +1,11 @@
+package sadek.doctorAppointments.appointmentsBooking.publicAPI;
+
+import org.springframework.modulith.ApplicationModule;
+import org.springframework.modulith.NamedInterface;
+import org.springframework.modulith.PackageInfo;
+
+@PackageInfo
+@NamedInterface("appointmentBooking.publicAPI")
+@ApplicationModule(type = ApplicationModule.Type.OPEN)
+public class MetaData {
+}
